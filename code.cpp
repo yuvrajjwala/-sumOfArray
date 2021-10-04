0a0beb11e4700e7511e49db5ebbe5bfcb58f7056
Yuvraj Jwala <74554809+yuvrajjwala@users.noreply.github.com>
2021-10-04 14:30:46 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,17 @@
+//CODE FOR ADDING ALL ELEMENT OF ARRAY
+#include <bits/stdc++.h>
+using namespacestd;
+
+int main (){
+  int n;cin>>n;
+  int arr;
+  cin>>arr;
+  int sum=0;
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+    sum+=a[i];
+  }
+  
+  cout<<sum<<"\n";
+}
+  
