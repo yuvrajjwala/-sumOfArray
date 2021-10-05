ed83531121a5e78b1ee34268a4e3eeac6629659e
Ankitsh-rtx <79033610+Ankitsh-rtx@users.noreply.github.com>
2021-10-05 11:15:29 +0530

    updated indentation

--- code.cpp
+++ code.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main (){
 
   int sum=0;
-  for(int i=0;i<n;i++){
-    cin>>arr[i];
-    sum+=arr[i];
+  for(int i = 0; i < n; i++ ){
+    cin >> arr[i];
+    sum += arr[i];
   }
   
-  cout<<sum<<"\n";
+  cout << sum << "\n";
 }
   
